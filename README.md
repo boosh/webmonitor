e453a5b1e39794872f1c08d4b7345d2dbee0cda8
Boosh <boosh@example.com>
2023-04-18 13:32:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Web monitor
+A simple go desktop app to poll a web page and notify you when it changes.
+
+## Usage
+1. Clone the repo
+2. Compile with `go build`
+3. Run it passing the url as an argument: `./web-monitor https://www.example.com`
+4. The app will launch then minimise itself. It will poll the page every `DELAY` seconds (see code) and pop up an alert when it changes.
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly/v2/extensions"
+)
+
+const DELAY = 30 // delay between checks in seconds
+
+func main() {
+	log.SetLevel(log.DebugLevel)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a URL to check")
+	}
+
+	url := os.Args[1]
+
+	println("Checking URL " + url + " for changes...")
+
+	myApp := app.New()
+	myApp.Settings().SetTheme(theme.LightTheme())
+	myWindow := myApp.NewWindow("Web Monitor")
+
+	myWindow.Resize(fyne.NewSize(300, 200))
+
+	var originalText string
+
+	go func() {
+		for {
+
+			if originalText == "" {
+				originalText = getPage(url)
+				log.Debugf("Original text: %s", originalText)
+				continue
+			}
+
+			time.Sleep(DELAY * time.Second)
+
+			pageText := getPage(url)
+			log.Debugf("Current text: %s", pageText)
+
+			if pageText != originalText {
+				log.Info("Web page change detected")
+				showAlert(myWindow, url)
+			}
+		}
+	}()
+
+	go func() {
+		time.Sleep(200 * time.Millisecond)
+		myWindow.Hide()
+	}()
+
+	myWindow.ShowAndRun()
+}
+
+func getPage(url string) string {
+	// Create a new collector
+	c := colly.NewCollector()
+
+	// Set User-Agent to simulate a browser request
+	extensions.RandomUserAgent(c)
+
+	// Variable to store the extracted text content
+	var pageText strings.Builder
+
+	// Callback for when visiting a HTML element
+	c.OnHTML("*", func(e *colly.HTMLElement) {
+		text := strings.TrimSpace(e.Text)
+		if text != "" {
+			pageText.WriteString(text)
+			pageText.WriteString("\n")
+		}
+	})
+
+	// Callback for error handling
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	// Start the web scraping by visiting the target URL
+	err := c.Visit(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return pageText.String()
+}
+
+func showAlert(window fyne.Window, url string) {
+	alert := dialog.NewInformation("Alert", "Web page has changed: "+url, window)
+	alert.Show()
+
+	window.Hide()
+	window.Show()
+}
