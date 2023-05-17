73365720bde8df1f852a39cae89c3986be8a1927
Boosh <boosh@example.com>
2023-05-17 10:40:42 +0100

    Support alerting if text exists

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -21,25 +22,32 @@ import (
 	"github.com/gocolly/colly/v2/extensions"
 )
 
-const DELAY = 30 // delay between checks in seconds
-
 func main() {
 	log.SetLevel(log.InfoLevel)
 
-	if len(os.Args) < 2 {
-		log.Fatal("Please provide a URL to check")
+	var textExists string
+	var delay uint
+
+	// Define flag arguments
+	flag.StringVar(&textExists, "text-exists", "", "Alert only if this text exists on the page")
+	flag.UintVar(&delay, "delay", 30, "Poll interval in seconds")
+
+	flag.Usage = usage
+
+	// Parse command-line flags
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 2 {
+		log.Fatal("Please provide a URL to check and MP3 file to play")
 		usage()
 		os.Exit(1)
 	}
 
-	urlStr := os.Args[1]
+	urlStr := args[0]
+	mp3Path := args[1]
 
-	mp3Path := ""
-	if len(os.Args) == 3 {
-		mp3Path = os.Args[2]
-	}
-
-	fmt.Printf("Will check URL "+urlStr+" for changes every %ds\n", DELAY)
+	fmt.Printf("Will check URL "+urlStr+" for changes every %ds\n", delay)
 
 	myApp := app.New()
 	myApp.Settings().SetTheme(theme.LightTheme())
@@ -63,56 +71,71 @@ func main() {
 
 	myWindow.Resize(fyne.NewSize(300, 200))
 
-	var originalText string
-
-	go func() {
-		for {
-			if originalText == "" {
-				originalText = getPage(urlStr)
-				log.Debugf("Original text: %s", originalText)
-				statusLabel.SetText(fmt.Sprintf("[%s] Initial page retrieved", time.Now().Format("2006-01-02 15:04:05")))
-				continue
-			}
-
-			time.Sleep(DELAY * time.Second)
-
-			pageText := getPage(urlStr)
-
-			if pageText == "" {
-				log.Warn("Empty page text")
-				statusLabel.SetText(fmt.Sprintf("[%s] Empty page text", time.Now().Format("2006-01-02 15:04:05")))
-				continue
-			}
-
-			log.Debugf("Current text: %s", pageText)
-
-			if pageText != originalText {
-				log.Info("Web page change detected")
-
-				statusLabel.SetText(fmt.Sprintf("[%s] Page has changed", time.Now().Format("2006-01-02 15:04:05")))
-
-				go func() {
-					showAlert(myWindow, urlStr)
-				}()
-
-				if mp3Path != "" {
-					log.Info("Playing sound")
-					playMp3(mp3Path)
-				}
-
-				originalText = pageText
-			} else {
-				log.Infof("[%s] Page unchanged", time.Now().Format("2006-01-02 15:04:05"))
-				statusLabel.SetText(fmt.Sprintf("[%s] Page unchanged", time.Now().Format("2006-01-02 15:04:05")))
-			}
-		}
-	}()
+	go pollForChanges(urlStr, mp3Path, delay, textExists, myWindow, statusLabel)
 
 	myWindow.ShowAndRun()
 }
 
 func usage() {
-	fmt.Println("Usage: web-monitor <URL> [MP3 file]")
+	fmt.Printf("Usage: %s [flags] <URL> <MP3 file>\n", os.Args[0])
+	fmt.Println("Poll a web page for changes and play the MP3 file when a change is detected")
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
+}
+
+// Poll a web page for changes and play an MP3 file when a change is detected
+func pollForChanges(urlStr string, mp3Path string, delay uint, textExists string, myWindow fyne.Window, statusLabel *widget.Label) {
+	var originalText string
+	var pageChanged bool
+
+	for {
+		if originalText == "" {
+			originalText = getPage(urlStr)
+			log.Debugf("Original text: %s", originalText)
+			statusLabel.SetText(fmt.Sprintf("[%s] Initial page retrieved", time.Now().Format("2006-01-02 15:04:05")))
+			continue
+		}
+
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		pageText := getPage(urlStr)
+
+		if pageText == "" {
+			log.Warn("Empty page text")
+			statusLabel.SetText(fmt.Sprintf("[%s] Empty page text", time.Now().Format("2006-01-02 15:04:05")))
+			continue
+		}
+
+		log.Debugf("Current text: %s", pageText)
+
+		pageChanged = false
+
+		if textExists != "" {
+			pageChanged = strings.Contains(strings.ToLower(pageText), strings.ToLower(textExists))
+		} else {
+			pageChanged = pageText != originalText
+		}
+
+		if pageChanged {
+			log.Info("Web page change detected")
+
+			statusLabel.SetText(fmt.Sprintf("[%s] Page has changed", time.Now().Format("2006-01-02 15:04:05")))
+
+			go func() {
+				showAlert(myWindow, urlStr)
+			}()
+
+			if mp3Path != "" {
+				log.Info("Playing sound")
+				playMp3(mp3Path)
+			}
+
+			originalText = pageText
+		} else {
+			log.Infof("[%s] Page unchanged", time.Now().Format("2006-01-02 15:04:05"))
+			statusLabel.SetText(fmt.Sprintf("[%s] Page unchanged", time.Now().Format("2006-01-02 15:04:05")))
+		}
+	}
 }
 
 // Play an MP3 file
